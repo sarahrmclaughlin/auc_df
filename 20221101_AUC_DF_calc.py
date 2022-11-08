2672e78677bb4146964bd25a8186df5398497703
Sarah R McLaughlin <49911270+sarahrmclaughlin@users.noreply.github.com>
2022-11-08 09:08:51 -0500

    Reorganize code and include for loop

--- 20221101_AUC_DF_calc.py
+++ 20221101_AUC_DF_calc.py
@@ -5,6 +5,7 @@ from datetime import time, tzinfo, timedelta
 import datetime as datetime
 
 from pyspark.sql import functions as f
+from functools import partial
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
@@ -13,7 +14,26 @@ from sklearn.metrics import roc_curve, auc
 import matplotlib.pyplot as plt
 from sklearn.pipeline import make_pipeline
 
+
+### -----------------------------------------------------------------------------------------------------# 
+### -----------------------------------------------------------------------------------------------------# 
+#Goal is to get a dataframe that looks like this (will then be able to run it by product):
+###Calc AUC for each period, period 
+
+eff_date_period AUC  product(colname)
+2022-07-01      0.5  sales 
+2022-08-01      0.8  sales
+2022-09-01      0.6  sales    
+
+### -----------------------------------------------------------------------------------------------------# 
+### -----------------------------------------------------------------------------------------------------# 
+
+
+
+
 # COMMAND ----------
+
+##EXAMPLE DF
 
 data = {'month': ['April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December', 'January', 'February', 'March'],
     'kpi': ['sales', 'sales quantity', 'sales', 'sales', 'sales', 'sales', 'sales', 'sales quantity', 'sales', 'sales', 'sales', 'sales'],
@@ -23,13 +43,79 @@ data = {'month': ['April', 'May', 'June', 'July', 'August', 'September', 'Octobe
     }
 df2 = pd.DataFrame(data)
 df2['my'] = pd.to_datetime(df2['financial_year'].astype(str)  + df2['month'], format='%Y%B')
+df2['my2'] = df2['my'].dt.strftime('%Y-%m-%d')
 df2.head()
 
 # COMMAND ----------
 
+# COMMAND ----------
+
+
+### Create a list of dates to loop over 
+
+
+my = [*df2['my2'].drop_duplicates()]
+my
+
+
+### -----------------------------------------------------------------------------------------------------# 
+### -----------------------------------------------------------------------------------------------------# 
+# ATTEMPT 1: Add For Loop -Does not work 
+
+def ck(product_):
+# def ck(product_, dt_period_):
+  temp=pd.DataFrame(columns=['eff_date_period','auc','product'])
+  temp_lst = []
+  
+  for month_ in my:
+      dftemp = df2.loc[(df2['kpi'] == product_)]
+
+      X = dftemp['prediction'].to_numpy()
+      y = dftemp['sub_renew'].to_numpy()
+
+    #   print(X, '\n', y)
+    #   print(type(X), '\n', type(y))
+
+      X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=1, stratify=y)
+      X_train = X_train.reshape(-1, 1)
+      X_test = X_test.reshape(-1, 1)
+
+      # Create the estimator - pipeline
+      pipeline = make_pipeline(StandardScaler(), LogisticRegression(random_state=1))
+
+      # Create training test splits using all features
+      pipeline.fit(X_train,y_train)
+      probs = pipeline.predict_proba(X_test)
+      fpr, tpr, thresholds = roc_curve(y_test, probs[:, 1], pos_label=1)
+      roc_auc = auc(fpr, tpr)
+
+
+      #temp=pd.DataFrame(columns=['eff_date_period','auc','product'])
+#       temp=temp.append({'eff_date_period': dt_period_,'auc':roc_auc,'product': product_},ignore_index=True)    
+      temp=temp.append({'eff_date_period': month_,'auc':roc_auc,'product': product_},ignore_index=True)
+      temp_lst = temp_lst.append(roc_auc)
+      print(temp_lst)
+      print(month_)
+      
+    #   return roc_auc
+      return temp
+
+
+# a_dte  = ck(product_ = "sales", dt_period_="2022-07-01")
+x_dte  = ck(product_ = "sales")
+# print(dte)
+#   b = ck(product_ = "sales", dt_period_="2022-08-01")
+#   c = ck(product_ = "sales", dt_period_="2022-09-01")
+
+### -----------------------------------------------------------------------------------------------------# 
+### -----------------------------------------------------------------------------------------------------# 
+
+
+
+# ATTEMPT 2,This works, but still need to concatenate at the end. Don't want to have to do this, would prefer to use a loop
 def ck(product_, dt_period_):
   dftemp = df2.loc[(df2['kpi'] == product_)]
-  dftemp.head()
+
   X = dftemp['prediction'].to_numpy()
   y = dftemp['sub_renew'].to_numpy()
   
@@ -49,29 +135,25 @@ def ck(product_, dt_period_):
   fpr, tpr, thresholds = roc_curve(y_test, probs[:, 1], pos_label=1)
   roc_auc = auc(fpr, tpr)
   
-  comb_df = pd.DataFrame({'Eff_date_period' : '1900-01-01','AUC': 99, 'colname' : 'a'}, index=[0])
-  #comb_df = pd.concat([comb_df,pd.DataFrame({'Eff_date_period' : [dt_period_],'AUC': [roc_auc], 'colname' : [product_]})], ignore_index=True)
+
+  temp=pd.DataFrame(columns=['eff_date_period','auc','product'])
+  temp=temp.append({'eff_date_period': dt_period_,'auc':roc_auc,'product': product_},ignore_index=True)                     
   
-  ##another attempt
-  comb_df.loc[len(comb_df)] = [dt_period_, roc_auc, product_]
-  comb_df.head()
-  
-  
-  return comb_df.head()
+#   return roc_auc
+  return temp #comb_df
 
 
-ck(product_ = "sales", dt_period_="2022-07-01")
-ck(product_ = "sales", dt_period_="2022-08-01")
+a2 = ck(product_ = "sales", dt_period_="2022-07-01")
+b = ck(product_ = "sales", dt_period_="2022-08-01")
+c = ck(product_ = "sales", dt_period_="2022-09-01")
+# a = ck(product_ = "sales", dt_period_="2022-08-01")
 
 # COMMAND ----------
 
 
-###Goal is to return results in a dataframe that looks like:
+  
 
-product dt_period roc_auc
-sales   2022-07-01 0.7
-sales   2022-08-01 0.8
-sales   2022-09-01 0.9 
+
 
 
 
